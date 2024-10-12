aaba5b1c65cbd2cc03f9a7f1e01488440b76783a
Javier García <develop@javier.games>
2024-10-12 09:07:31 +0900

    Updated URL

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://bio.site/javiergarcia';
+        iframe.src = 'https://bio.site/javier.games';
         /**
          * Use dev server is query params are present
          *
